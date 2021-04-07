33f6de3a50af6e62d33df3b7ea200354fe69832f
Sanjukta-Das22 <63544579+Sanjukta-Das22@users.noreply.github.com>
2021-04-07 20:47:44 +0530

    Add files via upload

---
+++ LoginAsAdmin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Banking.Models
+{
+    public class LoginAsAdmin
+    {
+        [Required]
+        public string LoginId { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ LoginController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Banking.Models;
+using BankingLib;
+
+namespace Banking.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Verify()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Verify(Login lg)
+        {
+            LoginDAL lgdal = new LoginDAL();
+            if (!lgdal.VerifyData(lg))
+            {
+                Response.Write("Login Failure");
+
+            }
+            else
+            {
+                var lglist = lgdal.GetLoginDetails();
+                var dt = lglist.Where(x => x.Account_No == lg.Account_No).Select(x => x.LastLogin);
+                
+                if(dt.First() == null)
+                {
+                    lg.LastLogin = DateTime.Now;
+                    lgdal.UpdateLoginDetails(lg);
+                    return RedirectToAction("ChangePassword");
+                }
+                else
+                {
+                    RegisterDAL rd = new RegisterDAL();
+                    RegisterModel rm = new RegisterModel();
+                    var rmlist= rd.PeekDetails();
+                    foreach(var item in rmlist)
+                    {
+                        if (item.Account_No == lg.Account_No)
+                        {
+                            rm.Account_Name = item.Account_Name;
+                            rm.Account_No = item.Account_No;
+                            rm.Address = item.Address;
+                            rm.Bank_Branch = item.Bank_Branch;
+                            rm.Email = item.Email;
+                            rm.IFSC = item.IFSC;
+                            rm.Mobile_No = item.Mobile_No;
+                            rm.Password = item.Password;
+                            Session["User"] = rm;
+                            break;
+                        }
+                    }
+                    
+                    return RedirectToAction("Index", "Customer");
+                   
+                }
+            }
+            return View();
+
+        }
+        [HttpGet]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+         [HttpPost]
+        public ActionResult ChangePassword(Login lg)
+        {
+            LoginDAL lgdal = new LoginDAL();
+            lgdal.changepassword(lg);
+            return RedirectToAction("Register", "Register");
+        }
+        public ActionResult LoginAsAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginAsAdmin(LoginAsAdmin obj)
+        {
+            bool isvalid = (obj.Password == "admin" && obj.LoginId == "admin");
+            if (isvalid)
+            {
+                return RedirectToAction("ShowAllCustomers", "Admin");
+            }
+           
+
+            return View();
+        }
+
+       
+    }
+}
